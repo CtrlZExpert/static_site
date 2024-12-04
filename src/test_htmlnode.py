3593598c75364ba0a9ab1bf05dd2d9fea5ac0be9
CtrlZExpert <vernett.cartwright@gmail.com>
2024-12-04 17:13:26 -0500

    B: Add Node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -48,6 +48,38 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "This is a paragraph of text.")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("p","This is a child.")
+        parent_node = ParentNode("section", [child_node])
+        self.assertEqual(parent_node.to_html(), "<section><p>This is a child.</p></section>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b","This is a grandchild.")
+        child_node = ParentNode("p", [grandchild_node])
+        parent_node = ParentNode("section",[child_node])
+        self.assertEqual(parent_node.to_html(), "<section><p><b>This is a grandchild.</b></p></section>")
+
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text")
+            ],
+        )
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
 
 
 if __name__ == "__main__":
